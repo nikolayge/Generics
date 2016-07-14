d9d588157e99e54092ae332d9928d3177127b50a
Nikolay Guentchev <nikolaygue@gmail.com>
2016-07-14 16:31:31 -0400

    Small fix in StackTest

--- src/generics/coffee/StackTest.java
+++ src/generics/coffee/StackTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class StackTest {
 	public static void main(String[] args) {
 		Stack<String> stack = new Stack<String>();
-		for (String s : "My dog has fleas".split(" "))
+		for (String s : "What are main topics about using Git as a source control".split(" "))
 			stack.push(s);
 		while (!stack.empty())
 			System.out.print(stack.pop() + " ");
